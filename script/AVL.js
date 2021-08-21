2a0b7d28849a344daf72bbcbda80cbd9fd5aa1bb
LiWaiYip <1155115790@link.cuhk.edu.hk>
2021-08-21 15:23:12 +0800

    Update AVL.js
    to make insertion O(log n) instead of brute-force O(n)

--- script/AVL.js
+++ script/AVL.js
@@ -2,21 +2,16 @@ class AVL {
     constructor() {
         this.root = null;
     }
-    getHeight(node) {
-        if (node == null) return 0;
-        return Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
-    }
     getBalanceFactor(node) {
-        /*var leftHeight = node.left == null ? 0 : node.left.height;
+        var leftHeight = node.left == null ? 0 : node.left.height;
         var rightHeight = node.right == null ? 0 : node.right.height;
-        return leftHeight - rightHeight;*/
-        return this.getHeight(node.left) - this.getHeight(node.right);
+        return leftHeight - rightHeight;
     }
-    changeHeight(node, delta) {
+    recalculateHeight(node) {
         if (node == null) return;
-        node.height += delta;
-        this.changeHeight(node.left, delta);
-        this.changeHeight(node.right, delta);
+        var leftHeight = node.left == null ? 0 : node.left.height;
+        var rightHeight = node.right == null ? 0 : node.right.height;
+        node.height = Math.max(leftHeight, rightHeight) + 1;
     }
     rightRotate(node) {
         var ans = new TreeNode(node.left.label, node.height);
@@ -24,15 +19,9 @@ class AVL {
         ans.left = node.left.left;
         node.left = node.left.right;
 
-        /*
-        node.height--;
-        this.changeHeight(node.right, 1);
-        this.changeHeight(ans.left, -1);
-
-        var leftHeight = ans.left == null ? 0 : ans.left.height;
-        var rightHeight = ans.right == null ? 0 : ans.right.height;
-        ans.height = Math.max(leftHeight, rightHeight) + 1;
-        */
+        this.recalculateHeight(node.left);
+        this.recalculateHeight(node.right);
+        this.recalculateHeight(node);
 
         return ans;
     }
@@ -42,15 +31,9 @@ class AVL {
         ans.right = node.right.right;
         node.right = node.right.left;
 
-        /*
-        node.height--;
-        this.changeHeight(node.left, 1);
-        this.changeHeight(ans.right, -1);
-
-        var leftHeight = ans.left == null ? 0 : ans.left.height;
-        var rightHeight = ans.right == null ? 0 : ans.right.height;
-        ans.height = Math.max(leftHeight, rightHeight) + 1;
-        */
+        this.recalculateHeight(node.left);
+        this.recalculateHeight(node.right);
+        this.recalculateHeight(node);
 
         return ans;   
     }
@@ -77,11 +60,8 @@ class AVL {
             node.right = this.insertHelper(node.right, val);
         }
         var balanceFactor = this.getBalanceFactor(node);
-        var leftHeight = node.left == null ? 0 : node.left.height;
-        var rightHeight = node.right == null ? 0 : node.right.height;
-        //console.log(node.label + " " + leftHeight + " " + rightHeight);
         if (Math.abs(balanceFactor) <= 1) {
-            node.height = Math.max(leftHeight, rightHeight) + 1;
+            this.recalculateHeight(node);
             return node;    
         }
         else { // need to do rotation
@@ -101,29 +81,5 @@ class AVL {
 
     insert(val) {
         this.root = this.insertHelper(this.root, val);
-        /*
-        if (this.root == null) {
-            this.root = new TreeNode(val);
-        }
-        else {
-            let prev = this.root;
-            let cur = this.root;
-
-            while (cur != null) {
-                prev = cur;
-                if (cur.label > val) {
-                    cur = cur.left;
-                }
-                else {
-                    cur = cur.right;
-                }
-            }
-
-            if (prev.label > val)
-                prev.left = new TreeNode(val);
-            else
-                prev.right = new TreeNode(val);
-        }
-        */
     }
 }
